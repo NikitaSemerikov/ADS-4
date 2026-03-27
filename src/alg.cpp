953e2affc4352464289fd8adffd30874b6fdc6aa
NikitaSemerikov <167699682+NikitaSemerikov@users.noreply.github.com>
2026-03-27 13:44:57 +0300

    25-ИСТ-3_Семериков_Лаб4.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,69 @@
 // Copyright 2021 NNTU-CS
+#include <cstddef>
+
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+    for (int i = 0; i < len; ++i) {
+        for (int j = i + 1; j < len; ++j) {
+            int sum = arr[i] + arr[j];
+            if (sum == value) {
+                ++count;
+            } else if (sum > value) {
+                break;
+            }
+        }
+    }
+    return count;
 }
+
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+    int left = 0;
+    int right = len - 1;
+    int count = 0;
+
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+
+        if (sum == value) {
+            ++count;
+            ++left;
+        } else if (sum < value) {
+            ++left;
+        } else {
+            --right;
+        }
+    }
+    return count;
 }
+
+static int binarySearch(const int *arr, int left, int right, int target) {
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target) {
+            return mid;
+        }
+        if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1;
+}
+
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+
+    for (int i = 0; i < len; ++i) {
+        int need = value - arr[i];
+        if (need < 0) {
+            break;
+        }
+
+        int pos = binarySearch(arr, i + 1, len - 1, need);
+        if (pos != -1) {
+            ++count;
+        }
+    }
+    return count;
 }
