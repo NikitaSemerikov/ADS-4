1c2147ba35d75bfddb4f1b89a2c1d8a2234f9a7e
NikitaSemerikov <167699682+NikitaSemerikov@users.noreply.github.com>
2026-03-27 13:59:02 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,65 @@
 // Copyright 2021 NNTU-CS
+#include <cstddef>
+
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+    for (int i = 0; i < len; ++i) {
+        for (int j = i + 1; j < len; ++j) {
+            int sum = arr[i] + arr[j];
+            if (sum == value) {
+                ++count;
+            } else if (sum > value) {
+                break;
+            }
+        }
+    }
+    return count;
 }
+
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+    int left = 0;
+    int right = len - 1;
+    int count = 0;
+
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+
+        if (sum == value) {
+            int lv = arr[left];
+            int rv = arr[right];
+
+            if (lv == rv) {
+                int n = right - left + 1;
+                count += n * (n - 1) / 2;
+                break;
+            }
+
+            int lc = 0;
+            while (left < right && arr[left] == lv) {
+                ++left;
+                ++lc;
+            }
+
+            int rc = 0;
+            while (right >= left && arr[right] == rv) {
+                --right;
+                ++rc;
+            }
+
+            count += lc * rc;
+        } else if (sum < value) {
+            ++left;
+        } else {
+            --right;
+        }
+    }
+
+    return count;
 }
-int countPairs3(int *arr, int len, int value) {
-  return 0;
-}
+
+static int binarySearchFirst(const int *arr, int left, int right, int target) {
+    int pos = -1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        volatile
